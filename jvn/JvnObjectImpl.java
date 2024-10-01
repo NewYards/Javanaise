25c978af36d2591bcbe375f72866529ba616200a
leonardv <valentin.leonard@outlook.fr>
2024-10-01 10:15:10 +0200

    Lock in JvnObjectImpl

--- jvn/JvnObjectImpl.java
+++ jvn/JvnObjectImpl.java
@@ -1,18 +1,22 @@
 package jvn;
 
 import java.io.Serializable;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class JvnObjectImpl implements JvnObject{
     int id;
     Serializable o;
     JvnServerImpl remoteServer;
     STATE state;
+    Lock lock;
 
     public JvnObjectImpl(int id, Serializable o, JvnServerImpl remoteServer){
         this.id = id;
         this.o = o;
         this.remoteServer = remoteServer;
         this.state = STATE.NL;
+        this.lock = new ReentrantLock();
     }
 
     @Override
@@ -36,6 +40,7 @@ public class JvnObjectImpl implements JvnObject{
 
     @Override
     public void jvnLockWrite() throws JvnException {
+        lock.lock();
         try {
             this.o = remoteServer.jvnLockWrite(this.id);
         } catch (Exception e) {
@@ -45,7 +50,7 @@ public class JvnObjectImpl implements JvnObject{
 
     @Override
     public void jvnUnLock() throws JvnException {
-        this.state = STATE.NL;
+        lock.unlock();
     }
 
     @Override
@@ -60,6 +65,7 @@ public class JvnObjectImpl implements JvnObject{
 
     @Override
     public void jvnInvalidateReader() throws JvnException {
+        lock.lock();
         if (this.state == STATE.R) {
             this.state = STATE.NL;
         }
@@ -70,6 +76,7 @@ public class JvnObjectImpl implements JvnObject{
 
     @Override
     public Serializable jvnInvalidateWriter() throws JvnException {
+        lock.lock();
         if (this.state == STATE.W) {
             this.state = STATE.NL;
         }
@@ -78,6 +85,7 @@ public class JvnObjectImpl implements JvnObject{
 
     @Override
     public Serializable jvnInvalidateWriterForReader() throws JvnException {
+        lock.lock();
         if (this.state == STATE.W) {
             this.state = STATE.RC;
         }
